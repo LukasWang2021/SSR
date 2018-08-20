b74cf42fd3aecfa65440ae80c6b38f2a4cf70eff
“zhengyu.shen” <zhengyu.shen@foresight-robotics.com>
2018-08-20 15:08:25 +0800

    fix error_code.h

--- Motion_controller/src/controller/src/version.cpp
+++ Motion_controller/src/controller/src/version.cpp
@@ -6,7 +6,7 @@
 #define VER_REVISION 5
   
 #define VER_BUILD_DATE "20180820"  
-#define VER_BUILD_TIME "14:54:11"    
+#define VER_BUILD_TIME "15:05:54"    
   
 #define VERSION_ALL  "2.0.5.20180820"  
   

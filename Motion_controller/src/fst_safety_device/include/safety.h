a8915fdc8acdd9fe39beabeb3fd7256994fae9b9
lucelujiaming <lujiaming@bitbucket.org>
2018-08-23 14:54:53 +0800

    Merge safety code

---
+++ Motion_controller/src/fst_safety_device/include/safety.h
@@ -0,0 +1,109 @@
+/**********************************************
+Copyright © 2016 Foresight-Robotics Ltd. All rights reserved.
+File:       safety.h
+Author:     Shuguo.Zhang Feng.Wu 
+Create:     20-Sep-2017
+Modify:     22-Sep-2017
+Summary:    dealing with safety board
+**********************************************/
+
+#define SAFETY_MAX_FRAME	8
+#define R_SAFETY_MAX_FRAME	SAFETY_MAX_FRAME
+#define W_SAFETY_MAX_FRAME	SAFETY_MAX_FRAME
+
+#define FIRST_FRAME		0
+#define SECOND_FRAME	1
+#define THIRD_FRAME		2
+#define FOURTH_FRAME	3
+#define FIFTH_FRAME		4
+#define SIXTH_FRAME		5
+#define	SEVENTH_FRAME	6
+#define EIGHTH_FRAME	7
+
+#define R_HB_FRAME		THIRD_FRAME    // modified by Feng.Wu
+#define	W_HB_FRAME		THIRD_FRAME    // modified by Feng.Wu
+#define NO_HB_TIMES		3	/* NO_HB_TIMES means the times that there is no heartbeat continually. Each time expends 50ms. If it is matched, the error, ERR_SAFETY_NOT_CONNECT is reported. */
+
+#define SAFETY_CMD_RECV 0X5F    // add by Feng.Wu
+#define SAFETY_CMD_SEND 0XF5    // add by Feng.Wu
+#define SAFETY_CORE_SEND    0X0100
+
+#define SAFETY_INPUT_FIRSTFRAME     0x0101
+#define SAFETY_INPUT_SECONDFRAME    0x0102
+
+#define SAFETY_OUTPUT_FIRSTFRAME    0x0201
+#define SAFETY_OUTPUT_SECONDFRAME	0x0202
+
+#define ERR_SAFETY_FILE_OPEN (unsigned long long int)0x00010002008E0001   /*can't open file when initializing safety board*/
+#define ERR_SAFETY_FILE_MAP (unsigned long long int)0x00010002008E0002   /*Mapping is failed.*/
+#define ERR_SAFETY_RECV_CMD (unsigned long long int)0x00010002008E0015   /*Command is not received from the safety board*/
+#define ERR_SAFETY_NOT_CONNECT (unsigned long long int)0x00010002008E0016   /*Safety board is not connected with, and there is not heartbeat from the safety board.*/
+#define ERR_SAFETY_RECV_DIFF (unsigned long long int)0x00010007008E0017   /*The data from two safety MCUs are different.*/
+#define ERR_SAFETY_PTHREAD_INIT (unsigned long long int)0x00000001008E001F   /*Mutex initialization is failed.*/
+#define ERR_SAFETY_PTHREAD_LOCK (unsigned long long int)0x00000001008E0020   /*Mutex lock is failed*/
+#define ERR_SAFETY_PTHREAD_UNLOCK (unsigned long long int)0x00000001008E0021   /*Mutex unlock is failed*/
+#define ERR_SAFETY_FRAME (unsigned long long int)0x00000001008E0029   /*The frame is out of range.*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+//  -----------------------------------------------------------------------
+//  Function:		getSafety
+//  Description:	get the value from safety board.
+//  In:				frame
+//					determin the data of which frame of 4 frames
+//  Out:			err
+//					if the error occur, err will save ERR_SAFETY_FRAME which
+//					indicates that the frame is wrong.
+//					if there is no error, err will present 0.
+//  Return:			the data gotten from the safety. However, when 0 returned,
+//					please check err. if there is no error, 0 returned is the
+//					valid data from the safety.
+//  -----------------------------------------------------------------------
+int getSafety(int frame, unsigned long long int *err);
+//  -----------------------------------------------------------------------
+//  Function:		setSafety
+//  Description:	set a value to the safety
+//  In:				data
+//					frame
+//  Out:
+//  Return:			the error
+//					0: no error
+//					ERR_SAFETY_FRAME: the frame is wrong.
+//  -----------------------------------------------------------------------
+unsigned long long int setSafety(int data, int frame);
+//  -----------------------------------------------------------------------
+//  Function:		openSafety
+//  Description:	open the interface of the safety, and initialize the 
+//					interface
+//  In: 
+//  Out:
+//  Return:			0: no error
+//					ERR_SAFETY_FILE_OPEN: the safety cann't be opened.
+//					ERR_SAFETY_FILE_MAP:  Failed to map the registers of safety.
+//  -----------------------------------------------------------------------
+unsigned long long int openSafety(void);
+void closeSafety(void);
+
+//  -----------------------------------------------------------------------
+//  Function:		autorunSafetyData
+//  Description:	this function will run one time per 50ms, and get data
+//					from and set value to the safety.
+//  In: 
+//  Out:
+//  Return:			0: no error
+//					ERR_SAFETY_NOT_CONNECT: SafetyBoard is not connected or 
+//					there is no heartbeat from safetyboard.
+//					ERR_SAFETY_RECV_CMD:	Command received is not from safety.
+//                  ERR_SAFETY_RECV_DIFF:   data from two MCUs are different.
+//  -----------------------------------------------------------------------
+unsigned long long int autorunSafetyData(void);
+
+//void safetyWriteDownload(void);
+//void safetySetSeq(char seq);
+//void safetyGetSeq(char *seq);
+//unsigned long long int safetyReadUpload(void);
+
+#ifdef __cplusplus
+}
+#endif 
--- Motion_controller/src/fst_safety_device/src/fst_safety_device.cpp
+++ Motion_controller/src/fst_safety_device/src/fst_safety_device.cpp
@@ -1,7 +1,52 @@
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include <memory.h>
+
+#include "safety.h"
 #include "fst_safety_device.h"
+#include "error_monitor.h"
+
+#define CROSS_PLATFORM
 
 
 using namespace fst_hal;
+
+/**
+ * @brief : get local ip address
+ *
+ * @return : the ip address in the form of string
+ */
+static std::string getLocalIP()
+{
+	int fd;
+    struct ifreq ifr;
+
+    char iface[] = "eth0";
+     
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+ 
+    //Type of address to retrieve - IPv4 IP address
+    ifr.ifr_addr.sa_family = AF_INET;
+ 
+    //Copy the interface name in the ifreq structure
+    strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
+ 
+    ioctl(fd, SIOCGIFADDR, &ifr);
+ 
+    close(fd);
+ 
+    //display result
+    //printf("%s - %s\n" , iface , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
+	std::string ret = inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr);
+	return ret;
+}
 
 
 FstSafetyDevice::FstSafetyDevice(int address):
@@ -15,11 +60,41 @@ FstSafetyDevice::FstSafetyDevice(int address):
 
 FstSafetyDevice::~FstSafetyDevice()
 {
-
+#ifdef CROSS_PLATFORM 
+    closeSafety();
+#endif
 }
 
 bool FstSafetyDevice::init()
 {
+    int iRet = 0 ;
+    
+    // enable safty
+    //valid_flag_ = true;
+    
+    // disable safty
+    // 
+    std::string str_addr = getLocalIP();
+	if(str_addr.substr(0,3) == "192")
+	{
+    	FST_INFO("Use Fake Safety");
+        valid_flag_ = false;
+	}
+    else
+	{
+    	FST_INFO("Use True Safety");
+	    valid_flag_ = true;
+	}
+    
+    memset((char*)&din_frm2_, 0, sizeof(din_frm2_));
+    memset((char*)&dout_frm2_, 0, sizeof(dout_frm2_));
+#ifdef CROSS_PLATFORM
+    iRet = openSafety();
+#endif
+	if(iRet != 0)
+	{
+        FST_ERROR("init FstSafetyDevice failed");
+	}
     return true;
 }
 
@@ -33,4 +108,290 @@ void FstSafetyDevice::updateThreadFunc()
 {
 
 }
+
+
+U32 FstSafetyDevice::getDIFrm2()
+{
+    return *(U32*)&din_frm2_;
+}
+U32 FstSafetyDevice::getDOFrm2()
+{
+    return *(U32*)&dout_frm2_;
+}
+
+bool FstSafetyDevice::isDIFrmChanged()
+{
+    static U32 pre_frame_data;
+    U32 frm_data = getDIFrm2();
+    if (pre_frame_data == frm_data)
+    {
+        return false;
+    }
+    
+    pre_frame_data = frm_data;
+    return true;
+}
+
+// char FstSafetyDevice::getDIDec()
+// {
+//    return din_frm2_.load().byte6.slowdown;
+// }
+char FstSafetyDevice::getDIOutage1()
+{
+    return din_frm2_.load().byte5.outage1;
+}
+char FstSafetyDevice::getDIOutage0()
+{
+    return din_frm2_.load().byte5.outage0;
+}
+
+char FstSafetyDevice::getDIBrake3()
+{
+    return din_frm2_.load().byte5.brake3;
+}
+char FstSafetyDevice::getDIBrake2()
+{
+    return din_frm2_.load().byte5.brake2;
+}
+char FstSafetyDevice::getDIBrake1()
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("getDIBrake1 Error \n");
+        return 0;
+    }
+    return din_frm2_.load().byte5.brake1;
+}
+char FstSafetyDevice::getDIDeadmanPanic()
+{
+    return din_frm2_.load().byte7.deadman_panic;
+}
+char FstSafetyDevice::getDIDeadmanNormal()
+{
+    return din_frm2_.load().byte7.deadman_normal;
+}
+
+char FstSafetyDevice::getDITPManual()
+{
+    return din_frm2_.load().byte6.usermode_man;
+}
+char FstSafetyDevice::getDITPLimitedManual()
+{
+    return din_frm2_.load().byte6.usermode_limit;
+}
+char FstSafetyDevice::getDITPAuto()
+{
+    return din_frm2_.load().byte6.usermode_auto;
+}
+//
+//qianjin add for user mode
+int FstSafetyDevice::getDITPUserMode()
+{
+    int val;
+
+    val = 0;
+    if( din_frm2_.load().byte6.usermode_auto) val |= 0x1;
+    if( din_frm2_.load().byte6.usermode_man) val |= 0x2;
+    if( din_frm2_.load().byte6.usermode_limit) val |= 0x4;
+	
+//	FST_INFO("getDITPUserMode: safety_interface_ :: din_frm2_: %08X", *(U32*)&din_frm2_);
+//	FST_INFO("getDITPUserMode: safety_interface_ :: val: %d", val);
+
+    if(val == 0x1) return 1;
+    if(val == 0x2) return 3;
+    if(val == 0x4) return 2;
+    return 0;
+}
+
+char FstSafetyDevice::getDITPEStop()
+{
+    return din_frm2_.load().byte7.tp_estop;
+}
+char FstSafetyDevice::getDILimitedStop()
+{
+    return din_frm2_.load().byte7.lmt_stop;
+}
+char FstSafetyDevice::getDIExtEStop()
+{
+    return din_frm2_.load().byte7.ext_estop;
+}
+char FstSafetyDevice::getDICore1Reset()
+{
+    return din_frm2_.load().byte6.core1_reset;
+}
+char FstSafetyDevice::getDICore1Alarm()
+{
+    return din_frm2_.load().byte8.core1_alarm;
+}
+char FstSafetyDevice::getDIAlarm()
+{
+    return din_frm2_.load().byte8.alarming;
+}
+char FstSafetyDevice::getDISafetyDoorStop()
+{
+    return din_frm2_.load().byte7.safetydoor_stop;
+}
+char FstSafetyDevice::getDOType0Stop()
+{
+    return dout_frm2_.load().byte5.core0_sw0;
+}
+U64 FstSafetyDevice::setDOType0Stop(char data)
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("setDOType0Stop Error set %c\n", data);
+        return SUCCESS;
+    }
+    SafetyBoardDOFrm2 out = dout_frm2_;
+    out.byte5.core0_sw0 = data;
+        FST_INFO("setSafety set 0x%X\n", *(int*)&out);
+    return setSafety(*(int*)&out, SAFETY_OUTPUT_SECONDFRAME);
+}
+
+char FstSafetyDevice::getDOType1Stop()
+{
+    return dout_frm2_.load().byte5.core0_sw1;
+}
+U64 FstSafetyDevice::setDOType1Stop(char data)
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("setDOType1Stop Error set %c\n", data);
+        return SUCCESS;
+    }
+    SafetyBoardDOFrm2 out = dout_frm2_;
+    out.byte5.core0_sw1 = data;
+        FST_INFO("setSafety set 0x%X\n", *(int*)&out);
+    return setSafety(*(int*)&out, SAFETY_OUTPUT_SECONDFRAME);
+}
+
+char FstSafetyDevice::getDOType2Stop()
+{
+    return dout_frm2_.load().byte5.core0_sw2;
+}
+U64 FstSafetyDevice::setDOType2Stop(char data)
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("setDOType2Stop Error set %c\n", data);
+        return SUCCESS;
+    }
+    SafetyBoardDOFrm2 out = dout_frm2_;
+    out.byte5.core0_sw2 = data;
+        FST_INFO("setDOType1Stop set 0x%X\n", *(int*)&out);
+    return setSafety(*(int*)&out, SAFETY_OUTPUT_SECONDFRAME);
+}
+
+char FstSafetyDevice::getDOSafetyStopConf()
+{
+    return dout_frm2_.load().byte5.safedoor_stop_config;
+}
+U64 FstSafetyDevice::setDOSafetyStopConf(char data)
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("setDOSafetyStopConf Error set %c\n", data);
+        return SUCCESS;
+    }
+    SafetyBoardDOFrm2 out = dout_frm2_;
+    out.byte5.safedoor_stop_config = data;
+        FST_INFO("setDOSafetyStopConf set 0x%X\n", *(int*)&out);
+    return setSafety(*(int*)&out, SAFETY_OUTPUT_SECONDFRAME);
+}
+
+char FstSafetyDevice::getDOExtEStopConf()
+{
+    return dout_frm2_.load().byte5.ext_estop_config;
+}
+U64 FstSafetyDevice::setDOExtEStopConf(char data)
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("setDOExtEStopConf Error set %c\n", data);
+        return SUCCESS;
+    }
+    SafetyBoardDOFrm2 out = dout_frm2_;
+    out.byte5.ext_estop_config = data;
+        FST_INFO("setSafety set 0x%X\n", *(int*)&out);
+    return setSafety(*(int*)&out, SAFETY_OUTPUT_SECONDFRAME);
+}
+
+char FstSafetyDevice::getDOLmtStopConf()
+{
+    return dout_frm2_.load().byte5.lmt_stop_config;
+}
+U64 FstSafetyDevice::setDOLmtStopConf(char data)
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("setDOLmtStopConf Error set %c\n", data);
+        return SUCCESS;
+    }
+    SafetyBoardDOFrm2 out = dout_frm2_;
+    out.byte5.lmt_stop_config = data;
+        FST_INFO("setSafety set 0x%X\n", *(int*)&out);
+    return setSafety(*(int*)&out, SAFETY_OUTPUT_SECONDFRAME);
+}
+
+void FstSafetyDevice::reset()
+{
+    setDOType0Stop(0);
+    setDOType1Stop(0);
+    setDOType2Stop(0);
+}
+
+
+U64 FstSafetyDevice::setSafetyHeartBeat()
+{
+    if (isSafetyValid() == false)
+    {
+        // FST_INFO("setSafetyHeartBeat Error \n");
+        return SUCCESS;
+    }
+    U64 result = autorunSafetyData(); 
+    if (result == SUCCESS)
+    {
+        //U32 data = getSafety(SAFETY_INPUT_FIRSTFRAME, &result);
+        //din_frm1_ = *(SafetyBoardDIFrm1*)&data;
+        //memcpy((char*)&din_frm1_, (char*)&data, sizeof(U32));
+        U32 data = getSafety(SAFETY_INPUT_SECONDFRAME, &result);
+        // FST_INFO("getSafety return 0x%X", data);
+        // printf("getSafety set 0x%X", data);
+        //din_frm2_ = *(SafetyBoardDIFrm2*)&data;
+        memcpy((char*)&din_frm2_, (char*)&data, sizeof(U32));
+        /*data = getSafety(SAFETY_OUTPUT_FIRSTFRAME, &result);*/
+        ////dout_frm1_ = *(SafetyBoardDOFrm1*)&data;
+        //memcpy((char*)&dout_frm1_, (char*)&data, sizeof(U32));
+        //data = getSafety(SAFETY_OUTPUT_SECONDFRAME, &result);
+       //// dout_frm2_ = *(SafetyBoardDOFrm2*)&data;
+        /*memcpy((char*)&dout_frm2_, (char*)&data, sizeof(U32));*/
+    }
+    return result;
+}
+
+bool FstSafetyDevice::isSafetyValid()
+{
+   return valid_flag_;
+}
+
+bool FstSafetyDevice::isSafetyAlarm()
+{
+    static char pre_alarm = 1;
+
+    char alarm = getDIAlarm();    
+    if (alarm != pre_alarm)
+    {
+        FST_INFO("cur alarm:%d, pre_alarm:%d\n",alarm, pre_alarm);
+    }
+
+    if ((pre_alarm == 0) && (alarm == 1))
+    {
+        pre_alarm = alarm;
+        return true;
+    }
+
+    pre_alarm = alarm;
+    return false;
+}
+
 

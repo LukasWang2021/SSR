760fa27cbf4f097020078057f5c477627a152aa2
mechanical <fengyun_1987@bitbucket.org>
2018-08-28 11:15:08 +0800

    #72 modified data struct and interface

--- Motion_controller/src/tp_comm/src/publish_reg.cpp
+++ Motion_controller/src/tp_comm/src/publish_reg.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void TpComm::handlePublishElementRegPr(Comm_Publish& package, int element_index, TpPublishElement& list_element)
 {
-    if(!encodePublishElement(package.element[element_index].data, MessageType_PrRegData_fields, list_element.data_ptr))
+    if(!encodePublishElement(package.element[element_index].data, MessageType_PrValue_fields, list_element.data_ptr))
     {
         FST_ERROR("handlePublishElementRegPr: failed to encode Pr");
     }
@@ -14,7 +14,7 @@ void TpComm::handlePublishElementRegPr(Comm_Publish& package, int element_index,
 
 void TpComm::handlePublishElementRegSr(Comm_Publish& package, int element_index, TpPublishElement& list_element)
 {
-    if(!encodePublishElement(package.element[element_index].data, MessageType_SrRegData_fields, list_element.data_ptr))
+    if(!encodePublishElement(package.element[element_index].data, MessageType_String_fields, list_element.data_ptr))
     {
         FST_ERROR("handlePublishElementRegSr: failed to encode Sr");
     }
@@ -22,7 +22,7 @@ void TpComm::handlePublishElementRegSr(Comm_Publish& package, int element_index,
 
 void TpComm::handlePublishElementRegMr(Comm_Publish& package, int element_index, TpPublishElement& list_element)
 {
-    if(!encodePublishElement(package.element[element_index].data, MessageType_MrRegData_fields, list_element.data_ptr))
+    if(!encodePublishElement(package.element[element_index].data, MessageType_Int32_fields, list_element.data_ptr))
     {
         FST_ERROR("handlePublishElementRegMr: failed to encode Mr");
     }
@@ -30,7 +30,7 @@ void TpComm::handlePublishElementRegMr(Comm_Publish& package, int element_index,
 
 void TpComm::handlePublishElementRegHr(Comm_Publish& package, int element_index, TpPublishElement& list_element)
 {
-    if(!encodePublishElement(package.element[element_index].data, MessageType_HrRegData_fields, list_element.data_ptr))
+    if(!encodePublishElement(package.element[element_index].data, MessageType_HrValue_fields, list_element.data_ptr))
     {
         FST_ERROR("handlePublishElementRegHr: failed to encode Hr");
     }
@@ -38,7 +38,7 @@ void TpComm::handlePublishElementRegHr(Comm_Publish& package, int element_index,
 
 void TpComm::handlePublishElementRegR(Comm_Publish& package, int element_index, TpPublishElement& list_element)
 {
-    if(!encodePublishElement(package.element[element_index].data, MessageType_RRegData_fields, list_element.data_ptr))
+    if(!encodePublishElement(package.element[element_index].data, MessageType_Double_fields, list_element.data_ptr))
     {
         FST_ERROR("handlePublishElementRegR: failed to encode R");
     }

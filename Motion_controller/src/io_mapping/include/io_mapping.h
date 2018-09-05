832602f4f8664e4feb4f816e358eef8be13a5409
lucelujiaming <lujiaming@bitbucket.org>
2018-09-05 11:28:48 +0800

    Add io_mapping

--- Motion_controller/src/io_mapping/include/io_mapping.h
+++ Motion_controller/src/io_mapping/include/io_mapping.h
@@ -1,22 +1,71 @@
 #ifndef IO_MAPPING_H
 #define IO_MAPPING_H
 
+#include<stdio.h>
+#ifdef WIN32
+#pragma warning(disable : 4786)
+#endif
+#include <memory.h>
+#include <string.h>
+#include<string>
+#include<vector>
+#include<map>
+#include <algorithm>
+using namespace std;
+
+#include "io_mapping_cJSON.h"
 
 #include "io_mapping_param.h"
 #include "common_log.h"
 
 namespace fst_ctrl
 {
+	
+#ifndef WIN32
+#define stricmp     strcasecmp
+#endif
+	
+typedef struct _IOMapJsonInfo
+{
+	int 		from;
+	int 		index;
+	char		module[128];
+	int 		to;
+}IOMapJsonInfo;
+
+typedef struct _IOMapVarInfo
+{
+	char		in[8];
+	char		out[8];
+}IOMapVarInfo;
+
 class IoMapping
 {
 public:
     IoMapping();
     ~IoMapping();
-
+	
+	int init();
+	map<string, string> getIOMapper() { return io_mapper; }
+	
+private:
+	int generateIOInfo(IOMapJsonInfo &objInfo, char * strIOType);
+	int parseIOObject(cJSON *jsonIObject, char * strIOType);
+	int parseIO(cJSON *jsonDI, char * strIOType);
+	int parseIOMap(char * data, IOMapVarInfo &varInfo);
+	int printIOMapper();
+	int appendSingleIOMapper(char *filename, IOMapVarInfo &varInfo);
+	
+	vector<string> split(string str,string pattern);
+	
+	void loadProgramsPath();
+	char * getProgramsPath();
 private:
     IoMappingParam* param_ptr_;
     fst_log::Logger* log_ptr_;
  
+	map<string, string> io_mapper;
+	std::string files_manager_data_path = "";
 };
 
 }
--- Motion_controller/src/io_mapping/src/io_mapping.cpp
+++ Motion_controller/src/io_mapping/src/io_mapping.cpp
@@ -1,6 +1,5 @@
 #include "io_mapping.h"
 
-
 using namespace fst_ctrl;
 
 
@@ -10,6 +9,8 @@ IoMapping::IoMapping():
 {
     log_ptr_ = new fst_log::Logger();
     param_ptr_ = new IoMappingParam();
+
+	loadProgramsPath();
 }
 
 IoMapping::~IoMapping()
@@ -17,4 +18,271 @@ IoMapping::~IoMapping()
 
 }
 
+int IoMapping::init()
+{
+	IOMapVarInfo varInfo ;
+	io_mapper.clear();
+#ifdef WIN32
+	// AI/AO
+	strcpy(varInfo.in , "ai") ;  strcpy(varInfo.out , "ao") ;
+	appendSingleIOMapper("data\\io\\io_mapping\\ai_ao_mapping.json", varInfo);
+	// DI/DO
+	strcpy(varInfo.in , "di") ;  strcpy(varInfo.out , "do") ;
+	appendSingleIOMapper("data\\io\\io_mapping\\di_do_mapping.json", varInfo);
+	// RI/RO
+	strcpy(varInfo.in , "ri") ;  strcpy(varInfo.out , "ro") ;
+	appendSingleIOMapper("data\\io\\io_mapping\\ri_ro_mapping.json", varInfo);
+	// SI/SO
+	strcpy(varInfo.in , "si") ;  strcpy(varInfo.out , "so") ;
+	appendSingleIOMapper("data\\io\\io_mapping\\si_so_mapping.json", varInfo);
+	// UI/UO
+	strcpy(varInfo.in , "ui") ;  strcpy(varInfo.out , "uo") ;
+	appendSingleIOMapper("data\\io\\io_mapping\\ui_uo_mapping.json", varInfo);
+    print_io_mapper();
+#else
+	char io_map_file_name[128];
+	// AI/AO
+	strcpy(varInfo.in , "ai") ;  strcpy(varInfo.out , "ao") ;
+	sprintf(io_map_file_name, "%s/data/io/io_mapping/ai_ao_mapping.json", 
+		getProgramsPath());
+	appendSingleIOMapper(io_map_file_name, varInfo);
+	// DI/DO
+	strcpy(varInfo.in , "di") ;  strcpy(varInfo.out , "do") ;
+	sprintf(io_map_file_name, "%s/data/io/io_mapping/di_do_mapping.json", 
+		getProgramsPath());
+	appendSingleIOMapper(io_map_file_name, varInfo);
+	// RI/RO
+	strcpy(varInfo.in , "ri") ;  strcpy(varInfo.out , "ro") ;
+	sprintf(io_map_file_name, "%s/data/io/io_mapping/ri_ro_mapping.json", 
+		getProgramsPath());
+	appendSingleIOMapper(io_map_file_name, varInfo);
+	// SI/SO
+	strcpy(varInfo.in , "si") ;  strcpy(varInfo.out , "so") ;
+	sprintf(io_map_file_name, "%s/data/io/io_mapping/si_so_mapping.json", 
+		getProgramsPath());
+	appendSingleIOMapper(io_map_file_name, varInfo);
+	// UI/UO
+	strcpy(varInfo.in , "ui") ;  strcpy(varInfo.out , "uo") ;
+	sprintf(io_map_file_name, "%s/data/io/io_mapping/ui_uo_mapping.json", 
+		getProgramsPath());
+	appendSingleIOMapper(io_map_file_name, varInfo);
+#endif
+	return 1;
+}
+
+
+void IoMapping::loadProgramsPath()
+{
+	files_manager_data_path = "";
+#ifdef WIN32
+    files_manager_data_path = std::string(DATA_PATH);
+#else
+    fst_parameter::ParamGroup param_;
+    param_.loadParamFile("/root/install/share/configuration/machine/programs_path.yaml");
+    param_.getParam("file_manager/data", files_manager_data_path);
+	printf("forgesight_load_programs_path: %s .\n", files_manager_data_path.c_str());
+#endif
+	
+}
+
+char * IoMapping::getProgramsPath()
+{
+	return (char *)files_manager_data_path.c_str();
+}
+
+
+int IoMapping::generateIOInfo(IOMapJsonInfo &objInfo, char * strIOType)
+{
+	char cTemp[128];
+	char cUpperType[16];
+    char *pUpper;
+	string strKey, strValue ;
+	
+    strcpy(cUpperType, strIOType);
+	pUpper = cUpperType;
+	while(*pUpper){ 
+		*pUpper = toupper(*pUpper); 
+		pUpper++; 
+	}
+  
+//	int iDIRange = objInfo.to - objInfo.from + 1 ;
+	for (int i = objInfo.from ; i <= objInfo.to ; i++)
+	{
+		memset(cTemp, 0x00, 128);
+		sprintf(cTemp, "%s[%d]", strIOType, i);
+		strKey.assign(cTemp);
+		
+        vector<string> result=split(objInfo.module, "/"); //use space to split
+		sprintf(cTemp, "root/IO/%s/%s/%s/%d", 
+			result[0].c_str(), result[2].c_str(), cUpperType, 
+			objInfo.index + i - objInfo.from);
+		strValue.assign(cTemp);
+
+		io_mapper.insert(
+			map<string, string>::value_type(strKey, strValue));
+		// Add revert element
+		io_mapper.insert(
+			map<string, string>::value_type(strValue, strKey));
+	}
+	return 1;
+}
+
+int IoMapping::parseIOObject(cJSON *jsonIObject, char * strIOType)
+{
+	IOMapJsonInfo objInfo ;
+	int numentries=0; // ,i=0,fail=0;
+	cJSON *child=jsonIObject->child;
+	
+	// printf("parseDI: cJSON_Array %s\n", jsonIObject->string);
+	while (child) 
+	{
+		numentries++; 
+		// printf("parseIOObject: cJSON_Array %s\n", child->string);
+		if(strcmp(child->string, "from") == 0)
+		{
+			objInfo.from = child->valueint ;
+		}
+		else if(strcmp(child->string, "index") == 0)
+		{
+			objInfo.index = child->valueint ;
+		}
+		else if(strcmp(child->string, "module") == 0)
+		{
+			strcpy(objInfo.module, child->valuestring) ;
+		}
+		else if(strcmp(child->string, "to") == 0)
+		{
+			objInfo.to = child->valueint ;
+		}
+		child=child->next;
+	}
+	
+	generateIOInfo(objInfo, strIOType);
+	return 1;
+}
+
+int IoMapping::parseIO(cJSON *jsonDI, char * strIOType)
+{
+	int numentries=0; // ,i=0,fail=0;
+	cJSON *child=jsonDI->child;
+	while (child) 
+		numentries++, child=child->next;
+
+	child=jsonDI->child;
+	while (child) // && !fail)
+	{
+		// printf("parseIO: cJSON_Array %s\n", child->string);
+		switch ((child->type)&255)
+		{
+		case cJSON_Number:	
+			// printf("cJSON_Number %d\n", child->valueint); 
+			break;
+		case cJSON_String:	
+			// printf("cJSON_String %s\n", child->valuestring); 
+			break;
+		case cJSON_Object:	
+			// printf("cJSON_Object\n"); 
+			parseIOObject(child, strIOType);
+			break;
+		}
+		child=child->next;
+	}
+	return 1;
+}
+
+int IoMapping::parseIOMap(char * data, IOMapVarInfo &varInfo)
+{
+	cJSON *json;
+	json=cJSON_Parse(data);
+	if(json == NULL)
+		return -1;
+	
+	cJSON *child=json->child;
+	while(child)
+	{
+		switch ((child->type)&255)
+		{
+		case cJSON_True:	
+			// printf("cJSON_True"); 
+			break;
+		case cJSON_Number:	
+			// printf("cJSON_Number %d\n", child->valueint); 
+			break;
+		case cJSON_String:	
+			// printf("cJSON_String %s\n", child->valuestring);
+			break;
+		case cJSON_Array:	
+			{
+				// printf("parseIOMap: cJSON_Array %s\n", child->string);
+				if(stricmp(child->string, varInfo.in) == 0)
+					parseIO(child, varInfo.in);
+				else if(stricmp(child->string, varInfo.out) == 0)
+					parseIO(child, varInfo.out);
+				break;
+			}
+		case cJSON_Object:	
+			// printf("cJSON_Object\n"); 
+			break;
+		}
+		child = child->next ;
+	}
+	cJSON_Delete(json);
+	return 1;
+}
+
+int IoMapping::printIOMapper()
+{
+	map<string, string>::iterator it;
+
+	it = io_mapper.begin();
+	
+	printf("\t\tobjThreadCntrolBlock->io_mapper has %d elements \n", 
+			io_mapper.size());
+
+	while(it != io_mapper.end())
+	{
+		// it->first;  // it->second;
+		printf("\t\t%s :: %s \n", 
+				it->first.c_str(), it->second.c_str());
+		it++;         
+	}
+	return 1;
+}
+
+int IoMapping::appendSingleIOMapper(
+		char *filename, IOMapVarInfo &varInfo)
+{
+	FILE *f;long len;char *data;
+
+	f=fopen(filename,"rb"); 
+	if(f)
+	{
+	    fseek(f,0,SEEK_END); len=ftell(f); fseek(f,0,SEEK_SET);
+	    data=(char*)malloc(len+1); fread(data,1,len,f); 
+		fclose(f);
+		parseIOMap(data, varInfo);
+		free(data);
+	}
+	return 1;
+}
+
+vector<string> IoMapping::split(string str,string pattern)
+{
+	string::size_type pos;
+	vector<string> result;
+	str+=pattern;
+	int size=str.size();
+
+	for(int i=0; i<size; i++)
+	{
+		pos=str.find(pattern,i);
+		if((int)pos<size)
+		{
+			string s=str.substr(i,pos-i);
+			result.push_back(s);
+			i=pos+pattern.size()-1;
+		}
+	}
+	return result;
+}
 

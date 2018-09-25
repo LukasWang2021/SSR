1fbfa981dd9e472d4c1bc7ffc1863739333461e4
mechanical <fengyun_1987@bitbucket.org>
2018-09-25 18:07:57 +0800

    #69 add switch step interface

--- Motion_controller/src/tp_comm/src/request_interpreter.cpp
+++ Motion_controller/src/tp_comm/src/request_interpreter.cpp
@@ -200,3 +200,27 @@ void TpComm::handleRequest0x000086F4(int recv_bytes)
     handleRequestPackage(0x000086F4, (void*)request_data_ptr, (void*)response_data_ptr, 
         recv_bytes, RequestMessageType_Void_fields, -1);
 }
+
+//"/rpc/interpreter/switchStep"
+void TpComm::handleRequest0x000140F0(int recv_bytes)
+{
+    // create object for request and response package
+    RequestMessageType_Int32* request_data_ptr = new RequestMessageType_Int32;
+    if(request_data_ptr == NULL)
+    {
+        ErrorMonitor::instance()->add(TP_COMM_MEMORY_OPERATION_FAILED);
+        FST_ERROR("handleRequest: can't allocate memory for request_data");
+        return;
+    }
+    ResponseMessageType_Uint64* response_data_ptr = new ResponseMessageType_Uint64;
+    if(response_data_ptr == NULL)
+    {
+        ErrorMonitor::instance()->add(TP_COMM_MEMORY_OPERATION_FAILED);
+        FST_ERROR("handleRequest: can't allocate memory for response_data");
+        delete request_data_ptr;
+        return;
+    }
+    
+    handleRequestPackage(0x000140F0, (void*)request_data_ptr, (void*)response_data_ptr, 
+        recv_bytes, RequestMessageType_Int32_fields, -1);
+}

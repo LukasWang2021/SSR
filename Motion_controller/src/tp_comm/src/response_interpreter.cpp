37f70280840a462f7581a5717255b557dfce1b78
mechanical <fengyun_1987@bitbucket.org>
2018-10-17 13:31:05 +0800

    #69 add start method for interpreter

--- Motion_controller/src/tp_comm/src/response_interpreter.cpp
+++ Motion_controller/src/tp_comm/src/response_interpreter.cpp
@@ -154,3 +154,37 @@ void TpComm::handleResponse0x000140F0(std::vector<TpRequestResponse>::iterator& 
         delete (ResponseMessageType_Uint64*)task->response_data_ptr;
     }
 }
+//"/rpc/interpreter/setStartMethod"
+void TpComm::handleResponse0x000056E4(std::vector<TpRequestResponse>::iterator& task, int& send_buffer_size)
+{
+    if(!encodeResponsePackage(task->hash, ResponseMessageType_Uint64_fields, task->response_data_ptr, send_buffer_size))
+    {
+        FST_ERROR("handleResponse: failed to encode response package");// send
+    }
+    if(task->request_data_ptr != NULL)
+    {
+        delete (RequestMessageType_Int32*)task->request_data_ptr;
+    }
+    if(task->response_data_ptr != NULL)
+    {
+        delete (ResponseMessageType_Uint64*)task->response_data_ptr;
+    }
+}
+
+//"/rpc/interpreter/getStartMethod"
+void TpComm::handleResponse0x00005624(std::vector<TpRequestResponse>::iterator& task, int& send_buffer_size)
+{
+    if(!encodeResponsePackage(task->hash, ResponseMessageType_Uint64_Int32_fields, task->response_data_ptr, send_buffer_size))
+    {
+        FST_ERROR("handleResponse: failed to encode response package");// send
+    }
+    if(task->request_data_ptr != NULL)
+    {
+        delete (RequestMessageType_Void*)task->request_data_ptr;
+    }
+    if(task->response_data_ptr != NULL)
+    {
+        delete (ResponseMessageType_Uint64_Int32*)task->response_data_ptr;
+    }
+}
+

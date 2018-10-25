27e4bf2a0168bb963badda151269b8f240f3b3e5
mechanical <fengyun_1987@bitbucket.org>
2018-10-25 17:02:38 +0800

    #73 reply invalid msg when receive a invalid hash at the beginning of msg

--- Motion_controller/src/tp_comm/src/response_common.cpp
+++ Motion_controller/src/tp_comm/src/response_common.cpp
@@ -37,3 +37,21 @@ void TpComm::handleResponse0x000147A5(std::vector<TpRequestResponse>::iterator& 
         delete (ResponseMessageType_Uint64_PublishTable*)task->response_data_ptr;
     }
 }
+
+void TpComm::handleResponse0xffffffff(std::vector<TpRequestResponse>::iterator& task, int& send_buffer_size)
+{
+    if(!encodeResponsePackage(task->hash, ResponseMessageType_Void_fields, task->response_data_ptr, send_buffer_size))
+    {
+        FST_ERROR("handleResponse0xffffffff: failed to encode response package");
+    }
+    if(task->request_data_ptr != NULL)
+    {
+        delete (RequestMessageType_Void*)task->request_data_ptr;
+    }
+    if(task->response_data_ptr != NULL)
+    {
+        delete (ResponseMessageType_Void*)task->response_data_ptr;
+    }
+}
+
+

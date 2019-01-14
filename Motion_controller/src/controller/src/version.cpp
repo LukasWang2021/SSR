1cde83633c701d5e265c21ac8bfaf2bdc71c3a22
“Feng <feng.wu@foresight-robotics.com>
2019-01-14 10:31:17 +0800

    update publish version 3.0

--- Motion_controller/src/controller/src/version.cpp
+++ Motion_controller/src/controller/src/version.cpp
@@ -4,13 +4,22 @@
 #include <string>
 #include <string.h>
 
+#define MAIN_VERSION 1
 
 char *get_version()
 {
     static char version_all[64] = "";
+
+#ifndef MAIN_VERSION
     sprintf(version_all, "%d.%d.%d.%s.%s", controller_VERSION_MAJOR,
             controller_VERSION_MINOR, controller_VERSION_PATCH,
             controller_BUILD_DATE,controller_VERSION_COMMIT);
+
+#else 
+    sprintf(version_all, "%d.%d", controller_VERSION_MAJOR,controller_VERSION_MINOR);
+
+#endif
+
     printf("controller_version:%s\n", version_all);
     return version_all;
 }  

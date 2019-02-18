26adbc397df9526d7f3262e74acebd5b7a48d5a3
lucelujiaming <lujiaming@bitbucket.org>
2019-02-18 08:54:16 +0800

    Add modebus address calculation

--- Motion_controller/src/controller/src/ipc_reg_manager.cpp
+++ Motion_controller/src/controller/src/ipc_reg_manager.cpp
@@ -53,7 +53,7 @@ void ControllerIpc::handleIpcSetMiValue(void* request_data_ptr, void* response_d
     fst_base::MiDataIpc* rq_data_ptr = static_cast<fst_base::MiDataIpc*>(request_data_ptr);
     bool* rs_data_ptr = static_cast<bool*>(response_data_ptr);
 
-    int addr = (rq_data_ptr->id) * 2 - 1;
+    int addr = 100 + (rq_data_ptr->id) * 2 - 1;
     uint16_t reg[2];
     reg[0] = (rq_data_ptr->value) & 0x00FF;
     reg[1] = (rq_data_ptr->value >>16) & 0x00FF;
@@ -70,7 +70,7 @@ void ControllerIpc::handleIpcSetMhValue(void* request_data_ptr, void* response_d
     fst_base::MhDataIpc* rq_data_ptr = static_cast<fst_base::MhDataIpc*>(request_data_ptr);
     bool* rs_data_ptr = static_cast<bool*>(response_data_ptr);
     
-    int addr = (rq_data_ptr->id) * 2 - 1;
+    int addr = 100 + (rq_data_ptr->id) * 2 - 1;
     uint16_t reg[2];
     reg[0] = (rq_data_ptr->value) & 0x00FF;
     reg[1] = (rq_data_ptr->value >>16) & 0x00FF;
@@ -144,12 +144,15 @@ void ControllerIpc::handleIpcGetMiValue(void* request_data_ptr, void* response_d
     fst_base::MiDataIpc* rs_data_ptr = static_cast<fst_base::MiDataIpc*>(response_data_ptr);
 
     uint16_t reg[2] = {0, 0}; 
+    *rq_data_ptr = 100 + (*rq_data_ptr) * 2 - 1;
 
     if (modbus_manager_ptr_->readInputRegs(0, *rq_data_ptr, 2, &reg[0]) == SUCCESS)
     {
         int32_t h_reg = reg[1]<<16;
         rs_data_ptr->value = reg[0] + h_reg;
         rs_data_ptr->id = *rq_data_ptr;
+		FST_INFO("handleIpcGetMiValue::rq_data_ptr        = %d", *rq_data_ptr);
+		FST_INFO("handleIpcGetMiValue::rs_data_ptr->value = %d", rs_data_ptr->value);
     }
     else
     {
@@ -163,12 +166,15 @@ void ControllerIpc::handleIpcGetMhValue(void* request_data_ptr, void* response_d
     fst_base::MiDataIpc* rs_data_ptr = static_cast<fst_base::MiDataIpc*>(response_data_ptr);
 
     uint16_t reg[2] = {0, 0}; 
+    *rq_data_ptr = 100 + (*rq_data_ptr) * 2 - 1;
 
     if (modbus_manager_ptr_->readHoldingRegs(0, *rq_data_ptr, 2, &reg[0]) == SUCCESS)
     {
         int32_t h_reg = reg[1]<<16;
         rs_data_ptr->value = reg[0] + h_reg;
         rs_data_ptr->id = *rq_data_ptr;
+		FST_INFO("handleIpcGetMhValue::rq_data_ptr        = %d", *rq_data_ptr);
+		FST_INFO("handleIpcGetMhValue::rs_data_ptr->value = %d", rs_data_ptr->value);
     }
     else
     {

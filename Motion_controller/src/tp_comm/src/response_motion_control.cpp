725cc4f8f1b8dc49cb20b50e46980f0f5da1833d
mechanical <fengyun_1987@bitbucket.org>
2019-03-08 12:33:27 +0800

    #69 add joint step interface

--- Motion_controller/src/tp_comm/src/response_motion_control.cpp
+++ Motion_controller/src/tp_comm/src/response_motion_control.cpp
@@ -957,3 +957,36 @@ void TpComm::handleResponse0x0000EC64(std::vector<TpRequestResponse>::iterator& 
         delete (ResponseMessageType_Uint64_Int32List*)task->response_data_ptr;
     }
 }
+
+//"/rpc/motion_control/axis_group/setJointManualStep"
+void TpComm::handleResponse0x00018470(std::vector<TpRequestResponse>::iterator& task, int& send_buffer_size)
+{
+    if(!encodeResponsePackage(task->hash, ResponseMessageType_Uint64_fields, task->response_data_ptr, send_buffer_size))
+    {
+        FST_ERROR("handleResponse: failed to encode response package");// send
+    }
+    if(task->request_data_ptr != NULL)
+    {
+        delete (RequestMessageType_Int32_DoubleList*)task->request_data_ptr;
+    }
+    if(task->response_data_ptr != NULL)
+    {
+        delete (ResponseMessageType_Uint64*)task->response_data_ptr;
+    }
+}
+//"/rpc/motion_control/axis_group/getJointManualStep"
+void TpComm::handleResponse0x00006D10(std::vector<TpRequestResponse>::iterator& task, int& send_buffer_size)
+{
+    if(!encodeResponsePackage(task->hash, ResponseMessageType_Uint64_DoubleList_fields, task->response_data_ptr, send_buffer_size))
+    {
+        FST_ERROR("handleResponse: failed to encode response package");// send
+    }
+    if(task->request_data_ptr != NULL)
+    {
+        delete (RequestMessageType_Int32*)task->request_data_ptr;
+    }
+    if(task->response_data_ptr != NULL)
+    {
+        delete (ResponseMessageType_Uint64_DoubleList*)task->response_data_ptr;
+    }
+}

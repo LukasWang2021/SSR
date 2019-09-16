641066f635af394d41694b6b3c96f9103e086fcd
mechanical <fengyun_1987@bitbucket.org>
2019-09-16 15:28:28 +0800

    use asynchronous transmission to send alarms to TP

--- Motion_controller/src/log_service/include/serverAlarmApi.h
+++ Motion_controller/src/log_service/include/serverAlarmApi.h
@@ -2,17 +2,24 @@
 #define ServerAlarmApi_H_2018_10_19_
 
 #include <string>
-#include "log_service_cJSON.h"
+#include <cjson/cJSON.h>
 #include <curl/curl.h>
-#include <list>
-using  std::list;
+#include <queue>
 using  std::string;
+using std::queue;
 #define ALARM_SERVER_URL "http://0.0.0.0:9003"
 #define CHUNCK_SIZE 256
+//#define PRINT_ENABLE
 
 struct MemoryStruct {
   char *memory;
   size_t size;
+};
+
+struct AlarmEvent {
+    unsigned long long int code;
+    struct timeval stamp;
+    std::string param;
 };
 
 class ServerAlarmApi
@@ -20,6 +27,7 @@ class ServerAlarmApi
 private:
     ServerAlarmApi();
     struct MemoryStruct m_chunk_;
+    pthread_t m_tid;
     char event_code_str_[32];  // 用于转换long long到string
     char time_str_[32];  // 字符串格式的时间
     bool enabled;
@@ -33,10 +41,18 @@ private:
     */
     char *constructEventInfo(unsigned long long event_code, string event_param);
     void parseResponse(char *response);
-    int post(const char *str_url, const char *str_post);
 
 public:
+    bool m_loop;
+    queue<char*> m_eventCodeQue;
+    std::vector<AlarmEvent> alarm_fifo_;
+    pthread_cond_t m_cond;
+    pthread_mutex_t m_lock;
+    pthread_mutex_t m_syncLock;
     static ServerAlarmApi *GetInstance();
+    long getTimestamp();
+    int post(const char *str_url, const char *str_post);
+    void printDebugInfo(string debugInfo);
     void clearUp();
     int sendOneAlarm(unsigned long long event_code, string event_param);
     int sendOneAlarm(unsigned long long event_code);

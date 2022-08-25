ecd6007751cb4d506113c2a836100e4b87e5ba16
wuym <youming.wu@rtimeman.com>
2022-08-25 17:47:56 +0800

    delete temp files

--- Motion_controller/build/embed_python/module/CMakeFiles/PYMODULES.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PYMODULES"
-  "pymodules"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PYMODULES.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
